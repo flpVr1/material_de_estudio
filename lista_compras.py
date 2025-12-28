e6a33d65cb9a013efb4adbf0267930ce3b3b9d81
flpVr1 <felipevergaraj7@gmail.com>
2025-12-28 19:35:13 -0300

    Actualización de lista_de_compras

--- lista_compras.py
+++ lista_compras.py
@@ -3,3 +3,45 @@
 #
 #
 #
+
+# Creación de la lista de compras vacía.
+mi_lista = []
+
+while True:
+
+    # Impresión del mensaje de bienvenida
+    print('=' * 30)
+    print('1. Agregar un producto')
+    print('2. Eliminar un producto')
+    print('3. Ver Lista')
+    print('4. Salir')
+    print('=' * 30)
+
+    # Almacenamiento de datos que ingresará el usuario
+    opcion = input('Qué deseas hacer?: ')
+
+    # Preguntar al usuario que desea hacer mostrando el menú
+    #
+    #
+    # Opción 1
+    if opcion == '1':
+        producto = input('Ingresa el nombre del producto: ')
+        mi_lista.append(producto)
+        print(f'Producto {producto} agregado a la lista de compras')
+    elif opcion == '2':
+        if mi_lista:
+            print('Lista de productos: ')
+            for i, producto in enumerate(mi_lista, 1):
+                print(f'{i}, {producto}')
+    elif opcion == '3':
+        if mi_lista:
+            print(f'Lista de productos:')
+            for i, producto in enumerate(mi_lista, 1):
+                print(f'{i}. {producto}')
+        else:
+            print('La lista está vacía.')
+    elif opcion == '4':
+        print('Hasta luego!')
+        break
+    else:
+        print('Opcíon inválida. Intenta nuevamente.')

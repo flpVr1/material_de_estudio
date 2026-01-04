5142564fe23d4a853645192282d069dc5a69fa37
flpVr1 <felipevergaraj7@gmail.com>
2026-01-03 21:21:15 -0300

    Actualización de Frames y campos de entrada y algunos estilos

--- calculadora_cientifica.py
+++ calculadora_cientifica.py
@@ -32,6 +32,29 @@ class CalculadoraApp:
         style = ttk.Style() # Se crea un objeto 'Style' para la apariencia
         style.theme_use('clam') # Aplica el tema 'clam' incorporado en Tkinter
 
+        # Configuración del color de fondo
+        self.root.configure(bg='#f0f0f0')
+
+        # Configuración de estilos para diferentes tipos de widgets
+        style.configure('TLabel', # Estilo para todas las etiquetas (label)
+                        background="#f0f0f0",
+                        font=('Arial', 10))
+        
+        style.configure('TButton', # Estilo para todos los botones
+                        font=('Arial', 10, 'bold'),
+                        padding=10)
+        
+        style.configure('TEntry', # Estilo para campos de entrada 'Entry'
+                        font=('Arial', 11),
+                        padding=5)
+        
+        # Configuración de colores DÍNAMICOS para botones (cambian al pasar el mouse)
+        style.map('TButton',
+                background=[('active', '#4CAF50'),
+                            ('!active', '#5cb85c')],
+                foreground=[('active', 'white'),
+                            ('!active', 'white')])
+
     # Función para la mostrar la interfaz
     def crear_interfaz(self):
         # Crea todos los elementos de la interfaz gráfica
@@ -41,6 +64,53 @@ class CalculadoraApp:
                            text='Calculadora Sencilla', # Texto que se muestra
                            font=('Arial', 16, 'bold'))
         titulo.pack(pady=20) # Etiqueta de ventana con 20px de espacio vertical
+
+        # Frame para campos de entrada (Frame es un contenedor para organizar widgets)
+        frame_entradas = ttk.Frame(self.root, #Crea un frame dentro de la ventana
+                                  padding=15) # 15px de espacio interno en todos los lados
+        
+        frame_entradas.pack(fill='x', # Coloca el Frame expandiéndolo horizontalmente
+                            padx=20) #20 px de espacio horizontal externo
+        
+        # Primer Número
+        ttk.Label(frame_entradas, # Creación de una etiqueta DENTRO del Frame
+                  text='Primer número: ').grid( # grid() organiza en filas y columnas
+                      row=0, # Fila 0 (Primera fila)
+                      column=0, # Columna 0 (Primera columna)
+                      sticky='w', # Alinea el texto a la izquierda (w de west)
+                      pady=5 # 5px de espacio vertical
+                  )
+        
+        entrada1 = ttk.Entry(frame_entradas, #Crea un campo de entrada de texto
+                             textvariable=self.numero1_var, # Vincula con la variable que almacena su valor
+                             font=('Arial', 11), # Fuente
+                             width=25) # Su ancho en caracteres
+        
+        entrada1.grid(row=0, # Fila 0
+                      column=1, # Columna 1 (Al lado de la etiqueta)
+                      pady=5, # 5px de espacio vertical
+                      padx=(10,0)) # 10px de espacio a la izquierda, 0 a la derecha 
+        
+        entrada1.focus()
+
+        # Segundo Número
+        ttk.Label(frame_entradas,
+                  text='Segundo número: ').grid(
+                    row=1,
+                    column=0,
+                    sticky='w',
+                    pady=5
+                  )
+        
+        entrada2 = ttk.Entry(frame_entradas,
+                            textvariable=self.numero2_var,
+                            font=('Arial', 11),
+                            width=25)
+        
+        entrada2.grid(row=1,
+                      column=1,
+                      pady=5,
+                      padx=(10,0))
 
 def main():
     # Función principal para iniciar la aplicación
